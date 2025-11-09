f98a1792f96154b33af7844823b725bebe25327a
Garima Tomar <144693520+Garimatomar0598@users.noreply.github.com>
2025-11-09 20:59:37 +0530

    Add user feedback form submission to WhatsApp
    This script handles the submission of a contact form by preventing the default action, collecting input values, encoding a message for WhatsApp, and opening a chat with a predefined phone number.

---
+++ js/userfeedback.js
@@ -0,0 +1,26 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Get input values
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let company = document.getElementById("company").value;
+  let phone = document.getElementById("phone").value;
+  let message = document.getElementById("message").value;
+
+  // Encode message
+  let whatsappMessage = `Hello Garima! 👋%0A
+  You have a new message from your Portfolio:%0A%0A
+  👩 Name: ${name}%0A
+  💼 Company: ${company}%0A
+  📧 Email: ${email}%0A
+  📞 Phone: ${phone}%0A
+  📝 Message: ${message}`;
+
+  // WhatsApp number with country code (e.g. 91XXXXXXXXXX)
+  let phoneNumber = "918816029116";
+
+  // Open WhatsApp chat in new tab
+  let whatsappURL = `https://wa.me/${phoneNumber}?text=${whatsappMessage}`;
+  window.open(whatsappURL, "_blank");
+});
